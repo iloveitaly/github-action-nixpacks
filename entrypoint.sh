559607b82fd40c72bf08c2f018fbcf2a54f92254
Michael Bianco <iloveitaly@gmail.com>
2024-03-21 11:22:02 -0600

    IFS fix and shell check

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,21 +13,21 @@ BUILD_CMD="nixpacks build $INPUT_CONTEXT"
 
 # Incorporate provided input parameters from actions.yml into the Nixpacks build command
 if [ -n "${INPUT_TAGS}" ]; then
-    IFS=$', \n' read -ra TAGS <<< "$INPUT_TAGS"
+    IFS=', '$'\n' read -ra TAGS <<< "$INPUT_TAGS"
     for tag in "${TAGS[@]}"; do
         BUILD_CMD="$BUILD_CMD --tag $tag"
     done
 fi
 
 if [ -n "${INPUT_LABELS}" ]; then
-    IFS=$', \n' read -ra LABELS <<< "$INPUT_LABELS"
+    IFS=', '$'\n' read -ra LABELS <<< "$INPUT_LABELS"
     for label in "${LABELS[@]}"; do
         BUILD_CMD="$BUILD_CMD --label $label"
     done
 fi
 
 if [ -n "${INPUT_PLATFORMS}" ]; then
-    IFS=$', \n' read -ra PLATFORMS <<< "$INPUT_PLATFORMS"
+    IFS=', '$'\n' read -ra PLATFORMS <<< "$INPUT_PLATFORMS"
     for platform in "${PLATFORMS[@]}"; do
         BUILD_CMD="$BUILD_CMD --platform $platform"
     done
@@ -35,23 +35,27 @@ fi
 
 # Add the Nix and Apt packages if specified
 if [ -n "${INPUT_PKGS}" ]; then
-    BUILD_CMD="$BUILD_CMD --pkgs '${INPUT_PKGS}'"
+    # Using an array to avoid issues if there are spaces in the input
+    IFS=', '$'\n' read -ra PKGS_ARR <<< "$INPUT_PKGS"
+    BUILD_CMD="$BUILD_CMD --pkgs '${PKGS_ARR[*]}'"
 fi
 
 if [ -n "${INPUT_APT}" ]; then
-    BUILD_CMD="$BUILD_CMD --apt '${INPUT_APT}'"
+    # Using an array to avoid issues if there are spaces in the input
+    IFS=', '$'\n' read -ra APT_ARR <<< "$INPUT_APT"
+    BUILD_CMD="$BUILD_CMD --apt '${APT_ARR[*]}'"
 fi
 
 # Execute the Nixpacks build command
 echo "Executing Nixpacks build command:"
-echo $BUILD_CMD
-eval $BUILD_CMD
+echo "$BUILD_CMD"
+eval "$BUILD_CMD"
 
 # Conditionally push the images based on the 'push' input
 if [[ "$INPUT_PUSH" == "true" ]]; then
     for tag in "${TAGS[@]}"; do
         echo "Pushing Docker image: $tag"
-        docker push $tag
+        docker push "$tag"
     done
 fi
 
